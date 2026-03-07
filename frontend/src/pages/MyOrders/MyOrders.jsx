22f67c8270f44e6d05518565dc6e592ad465f53c
khushi-7488 <khushiitiwari88@gmail.com>
2026-03-07 23:21:06 +0530

    build myorders route

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -34,9 +34,21 @@ const MyOrders = () => {
           return (
             <div key={index} className="my-orders-order">
               <img src={assets.parcel_icon} alt="" />
-              <p>{order.items.map((item, index)=>{
-                
-              })}</p>
+              <p>
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + " x " + item.quantity;
+                  } else {
+                    return item.name + " x " + item.quantity + ",";
+                  }
+                })}
+              </p>
+              <p>${order.amount}</p>
+              <p>Items: {order.items.length}</p>
+              <p>
+                <span>&#x25cf</span> <b>{order.status}</b>
+              </p>
+              <button>Track Order</button>
             </div>
           );
         })}
