9f59a9eca153759d0db5a20fc96a58d0c802d952
khushi-7488 <khushiitiwari88@gmail.com>
2026-03-09 21:21:30 +0530

    build a fullstack food delivery app

--- admin/src/pages/Orders/Order.jsx
+++ admin/src/pages/Orders/Order.jsx
@@ -23,6 +23,16 @@ const Order = ({ url }) => {
     fetchAllOrders();
   }, []);
 
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(url + "/api/order/status", {
+      orderId,
+      status: event.target.value,
+    });
+    if( response.data.success){
+      await fetchAllOrders();
+    }
+  };
+
   return (
     <div className="order add">
       <h3>Order Page</h3>
@@ -60,7 +70,10 @@ const Order = ({ url }) => {
               </div>
               <p>Items: {order.items.length}</p>
               <p>${order.amount}</p>
-              <select>
+              <select
+                onChange={() => statusHandler(event, order._id)}
+                value={order.status}
+              >
                 <option value="Food processing">Food processing</option>
                 <option value="Out for delivery">Out for delivery</option>
                 <option value="Delivered">Delivered</option>
